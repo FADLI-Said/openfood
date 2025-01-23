f13b6d5becf3d2a7269adf476fcecd0ce6940e8e
FADLI Saïd <saidfadli224@gmail.com>
2025-01-23 15:27:34 +0100

    Reste le design

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,14 +2,22 @@ let urlParams = new URLSearchParams(window.location.search);
 item_code = urlParams.get("barCode")
 
 if (item_code == null || item_code == "") {
-    item_code = `5997523311230`
-  }
-  
+    item_code = "5997523311230"
+}
+
+document.getElementById("home").addEventListener("click", function () {
+    item_code = "5997523311230"
+    console.log(item_code);
+    window.location.href = `?barCode=${item_code}`
+})
+
+
 
 fetch(`https://world.openfoodfacts.org/api/v3/product/${item_code}`)
     .then(reponse => reponse.json())
     .then(data => {
         console.log(data);
+
         if (data.status == "failure") {
             document.getElementById("product").innerHTML = `
             <h1>Le produit n'existe pas ou le code-barres est incorrect</h1>
@@ -168,11 +176,27 @@ fetch(`https://world.openfoodfacts.org/api/v3/product/${item_code}`)
 
         let novaNote = data.product.nova_group
         let nbNovaMarkers = ""
+
         if (data.product.nova_groups_markers != null && data.product.nova_group != 1) {
             nbNovaMarkers = data.product.nova_groups_markers[novaNote].length + " marqueurs d'ultra-transformation"
         } else {
             nbNovaMarkers = ""
         }
+
+        let countrie = ""
+
+        if (data.product.countries_tags.length > 10) {
+            for (let i = 0; i < 5; i++) {
+                if (data.product.countries_tags[i].map(countries_tags => countries_tags.startsWith("en:"))) {
+                    countries_tags.replace("en:", "")
+                }
+                countrie += data.product.countries_tags[i].substring(3) + ", "
+            }
+        }
+
+
+        console.log(countrie);
+
 
         document.getElementById("product").innerHTML = `
         <img class="col-lg-4 bg-white"
@@ -202,11 +226,11 @@ fetch(`https://world.openfoodfacts.org/api/v3/product/${item_code}`)
             <p class="mt-3"><span class="fw-bold">Magasins </span> : ${data.product.stores}</p>
             <!-- stores_tags -->
 
-            <p class="mt-3"><span class="fw-bold">Pays de vente </span> : ${data.product.countries}</p>
+            <p class="mt-3"><span class="fw-bold">Pays de vente </span> : ${data.product.countries_tags.map(countries_tags => countries_tags.startsWith("en:") ? countries_tags.replace("en:", "") : countries_tags).join(", ")}</p>
             <!-- countries_hierarchy -->
         </div>
 
-        <div class="bg-white mx-0 mt-4 row p-3" id="score">
+        <div class="bg-white mx-0 mt-4 row p-0" id="score">
             <h2>Correspondance avec vos préférences</h2>
 
             <div class="d-lg-flex justify-content-between">
@@ -217,7 +241,7 @@ fetch(`https://world.openfoodfacts.org/api/v3/product/${item_code}`)
                             alt="Nutri-Score D">
                     </div>
                     <div class="col-lg-8  nutriscore">
-                        <p class="fs-6 orange fw-bold ${colorText}">Nutri-Score <span class="text-uppercase">${data.product.nutriscore_grade == "unknown" ? "Inconnu": data.product.nutriscore_grade}</span></p>
+                        <p class="fs-6 orange fw-bold ${colorText}">Nutri-Score <span class="text-uppercase">${data.product.nutriscore_grade == "unknown" ? "Inconnu" : data.product.nutriscore_grade}</span></p>
                         <p>${qualityNutri}</p>
                     </div>
                     <!--2023 - grade = d -->
@@ -247,83 +271,82 @@ fetch(`https://world.openfoodfacts.org/api/v3/product/${item_code}`)
                             <!-- grade -->
                     </div>
                     <div class="col-lg-7 nutriscore">
-                        <p class="fs-6 fw-bold ${greenScoreColor}">Green-Score <span class="text-uppercase">${data.product.ecoscore_grade == "unknown" ? "inconnu": data.product.ecoscore_grade}</span></p>
+                        <p class="fs-6 fw-bold ${greenScoreColor}">Green-Score <span class="text-uppercase">${data.product.ecoscore_grade == "unknown" ? "inconnu" : data.product.ecoscore_grade}</span></p>
                         <p>${ecoText}</p>
                     </div>
                     <!--2023 - grade = d -->
                 </div>
             </div>
         </div>
-        `          
+        `
     })
 
-    document.querySelector("#scan_button").addEventListener("click", function () {
-        let selectedDeviceId;
-        // document.querySelector("#video_container").classList.remove('visually-hidden')
-        const codeReader = new ZXing.BrowserMultiFormatReader();
-        console.log("ZXing code reader initialized");
-        codeReader
-          .listVideoInputDevices()
-          .then((videoInputDevices) => {
+document.querySelector("#scan_button").addEventListener("click", function () {
+    let selectedDeviceId;
+    // document.querySelector("#video_container").classList.remove('visually-hidden')
+    const codeReader = new ZXing.BrowserMultiFormatReader();
+    console.log("ZXing code reader initialized");
+    codeReader
+        .listVideoInputDevices()
+        .then((videoInputDevices) => {
             const sourceSelect = document.getElementById("sourceSelect");
             selectedDeviceId = videoInputDevices[0].deviceId;
             if (videoInputDevices.length >= 1) {
-              videoInputDevices.forEach((element) => {
-                const sourceOption = document.createElement("option");
-                sourceOption.text = element.label;
-                sourceOption.value = element.deviceId;
-                sourceSelect.appendChild(sourceOption);
-              });
-              sourceSelect.onchange = () => {
-                selectedDeviceId = sourceSelect.value;
-              };
-      
-              const sourceSelectPanel = document.getElementById("sourceSelectPanel");
-              sourceSelectPanel.style.display = "block";
-              const video_container = document.querySelector("#video_container");
-              video_container.style.display = "block";
-              document.querySelector("#button_container").style.display = "block";
+                videoInputDevices.forEach((element) => {
+                    const sourceOption = document.createElement("option");
+                    sourceOption.text = element.label;
+                    sourceOption.value = element.deviceId;
+                    sourceSelect.appendChild(sourceOption);
+                });
+                sourceSelect.onchange = () => {
+                    selectedDeviceId = sourceSelect.value;
+                };
+
+                const sourceSelectPanel = document.getElementById("sourceSelectPanel");
+                sourceSelectPanel.style.display = "block";
+                const video_container = document.querySelector("#video_container");
+                video_container.style.display = "block";
+                document.querySelector("#button_container").style.display = "block";
             }
             document.getElementById("startButton").addEventListener("click", () => {
-              codeReader.decodeFromVideoDevice(
-                selectedDeviceId,
-                "video",
-                (result, err) => {
-                  if (result) {
-                    
-                    console.log(result);
-                    document.getElementById("result").textContent = result.text;
-                      document.querySelector("#search_content").value = result.text;
-                      
-                    const son = new Audio();
-                    son.src = "../../bip_sound.mp3";
-                    son.play().then(() => {
-                      setTimeout(() => {
-                      document.getElementById("search_button").click()
-                    }, "1000");
-                      
-                    })
-                    
-                  }
-                  if (err && !(err instanceof ZXing.NotFoundException)) {
-                    console.error(err);
-                    document.getElementById("result").textContent = err;
-                  }
-                }
-              );
-      
-              console.log(
-                `Started continous decode from camera with id ${selectedDeviceId}`
-              );
+                codeReader.decodeFromVideoDevice(
+                    selectedDeviceId,
+                    "video",
+                    (result, err) => {
+                        if (result) {
+
+                            console.log(result);
+                            document.getElementById("result").textContent = result.text;
+                            document.querySelector("#search_content").value = result.text;
+
+                            const son = new Audio();
+                            son.src = "../../bip_sound.mp3";
+                            son.play().then(() => {
+                                setTimeout(() => {
+                                    document.getElementById("search_button").click()
+                                }, "1000");
+
+                            })
+
+                        }
+                        if (err && !(err instanceof ZXing.NotFoundException)) {
+                            console.error(err);
+                            document.getElementById("result").textContent = err;
+                        }
+                    }
+                );
+
+                console.log(
+                    `Started continous decode from camera with id ${selectedDeviceId}`
+                );
             });
             document.getElementById("resetButton").addEventListener("click", () => {
-              codeReader.reset();
-              document.getElementById("result").textContent = "";
-              console.log("Reset.");
+                codeReader.reset();
+                document.getElementById("result").textContent = "";
+                console.log("Reset.");
             });
-          })
-          .catch((err) => {
+        })
+        .catch((err) => {
             console.error(err);
-          });
-      });
-      
+        });
+});
